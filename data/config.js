de58ee5c08f46c4ea5cd4cecf591eaf730be0215
elascalla <erick.lascalla@gmail.com>
2019-11-17 16:19:32 -0400

    chore: fix config.js

---
+++ data/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	githubUser: 'elascalla',
+	defaultTitle: 'Hello!',
+	logo: 'https://avatars1.githubusercontent.com/u/421191?v=4',
+	author: 'Erick Lascalla',
+	url: 'https://erickdev.com',
+	legalName: 'Erick Anderson Lascalla Gomes',
+	defaultDescription: 'Senior FullStack Java Developer and FullStack JavaScript Developer with NodeJS, ReactJS and React Native Technologies',
+	socialLinks: {
+		twitter: 'http://www.twitter.com/elascalla',
+		github: 'https://github.com/elascalla',
+		linkedin: 'https://www.linkedin.com/in/erick-gomes/',
+		instagram: 'https://instagram.com/elascalla',
+		youtube: 'https://www.youtube.com/user/elascalla'
+	},
+	googleAnalyticsID: 'UA-144087925-1',
+	themeColor: '#6b63ff',
+	backgroundColor: '#6b63ff',
+	social: {
+		facebook: 'appId',
+		twitter: '@elascalla',
+		github: 'elascalla',
+		telegram: 'elascalla',
+	},
+	address: {
+		city: 'Campo Grande',
+		region: 'Mato Grosso do Sul',
+		country: 'Brasil',
+		zipCode: '79060280',
+	},
+	contact: {
+		email: 'erick.lascalla@gmail.com',
+		phone: '+55 67 99231-6533',
+	},
+	foundingDate: '2019',
+}
